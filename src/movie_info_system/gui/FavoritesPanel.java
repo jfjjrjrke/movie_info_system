717eeefd9d7c54280e46583c3f9a07b852ddef34
jihyeon-u <jihyeon-u@localhost>
2025-06-01 15:24:54 +0900

    gui 코드 업로

--- src/movie_info_system/gui/FavoritesPanel.java
+++ src/movie_info_system/gui/FavoritesPanel.java
@@ -1,16 +1,36 @@
 package movie_info_system.gui;
 
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
 
 public class FavoritesPanel extends JPanel {
 
-	private static final long serialVersionUID = 1L;
+	private DefaultListModel<String> listModel;
+	private JList<String> favoriteList;
+	private JButton removeButton;
 
-	/**
-	 * Create the panel.
-	 */
 	public FavoritesPanel() {
+		setLayout(new BorderLayout());
+		setBorder(BorderFactory.createTitledBorder("즐겨찾기 목록"));
 
+		listModel = new DefaultListModel<>();
+		favoriteList = new JList<>(listModel);
+		add(new JScrollPane(favoriteList), BorderLayout.CENTER);
+
+		removeButton = new JButton("삭제");
+		add(removeButton, BorderLayout.SOUTH);
+
+		removeButton.addActionListener(e -> {
+			int index = favoriteList.getSelectedIndex();
+			if (index != -1) {
+				listModel.remove(index);
+			}
+		});
 	}
 
+	public void addFavorite(String movieTitle) {
+		if (!listModel.contains(movieTitle)) {
+			listModel.addElement(movieTitle);
+		}
+	}
 }
--- src/movie_info_system/gui/MovieDetailPanel.java
+++ src/movie_info_system/gui/MovieDetailPanel.java
@@ -1,16 +1,37 @@
 package movie_info_system.gui;
 
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
 
 public class MovieDetailPanel extends JPanel {
 
-	private static final long serialVersionUID = 1L;
+	private JLabel titleLabel;
+	private JLabel dateLabel;
+	private JLabel ratingLabel;
+	private JTextArea overviewArea;
 
-	/**
-	 * Create the panel.
-	 */
 	public MovieDetailPanel() {
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		setBorder(BorderFactory.createTitledBorder("영화 상세 정보"));
 
+		titleLabel = new JLabel("제목: ");
+		dateLabel = new JLabel("개봉일: ");
+		ratingLabel = new JLabel("평점: ");
+		overviewArea = new JTextArea(5, 20);
+		overviewArea.setLineWrap(true);
+		overviewArea.setWrapStyleWord(true);
+		overviewArea.setEditable(false);
+
+		add(titleLabel);
+		add(dateLabel);
+		add(ratingLabel);
+		add(new JScrollPane(overviewArea));
 	}
 
+	public void setMovieDetail(String title, String date, String rating, String overview) {
+		titleLabel.setText("제목: " + title);
+		dateLabel.setText("개봉일: " + date);
+		ratingLabel.setText("평점: " + rating);
+		overviewArea.setText(overview);
+	}
 }
